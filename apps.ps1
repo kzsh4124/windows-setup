77d5005e8f3e4ad2f426d2b8ac8085a652a1490a
kzsh4124 <kazushi.02140413@gmail.com>
2024-10-17 19:28:49 +0900

    update

--- apps.ps1
+++ apps.ps1
@@ -13,5 +13,17 @@ winget install -e --id Valve.Steam
 winget install -e --id KiCad.KiCad
 winget install -e --id Google.JapaneseIME
 winget install -e --id Git.Git
+winget install -e --id Unity.UnityHub
+winget install -e --id Neovim.Neovim
+winget install -e --id FilesCommunity.Files
+winget install -e --id yt-dlp.yt-dlp
+
+
+
+echo "git settings"
+
+git config --global user.name "kzsh4124"
+git config --global user.email "ucan-lab@example.com"
+
 
 Set-ExecutionPolicy Restricted
